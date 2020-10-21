904f0493e26afe729ebf912d2adf0d62c38c5439
aledecos <aledecos@users.noreply.github.com>
2020-10-20 18:01:27 -0600

    Updates

---
+++ precache-manifest.9db38300a8b51105c687f57123d5f38a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a159ded25aad9af5455cff35b0ae918b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e1aa4ab5ac1a72d8a84",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "9b98f69dbdddbce4e8a3",
+    "url": "/static/css/main.63e47757.chunk.css"
+  },
+  {
+    "revision": "0e1aa4ab5ac1a72d8a84",
+    "url": "/static/js/2.56c5df2a.chunk.js"
+  },
+  {
+    "revision": "78fd0f62364854e39c53711d027f73a3",
+    "url": "/static/js/2.56c5df2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b98f69dbdddbce4e8a3",
+    "url": "/static/js/main.ae252e7a.chunk.js"
+  },
+  {
+    "revision": "5f91add4f6025a165cfb",
+    "url": "/static/js/runtime-main.29dc7578.js"
+  }
+]);
