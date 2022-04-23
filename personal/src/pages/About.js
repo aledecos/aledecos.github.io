6cda0480a135eadf2909e466c8f663a96a7a2468
aledecos <aledecos@users.noreply.github.com>
2022-04-23 00:45:54 -0600

    new nav

--- personal/src/pages/About.js
+++ personal/src/pages/About.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import Navsection from '../components/Header/Navsection'
 import "../Styles/About.css"
+import Header from "../components/Header"
 
 function About() {
   return (
       <>
-        <Navsection className="header"/>
+        <Header/>
         <section className="aboutus">
             <div className="title">
                     Welcome
             </div>
         </section>
+        
   
 
         
