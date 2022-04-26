a10f91c7833913c6f866123901e64501383c2076
aledecos <aledecos@users.noreply.github.com>
2022-04-26 00:07:54 -0600

    fix align bug

--- personal/src/components/Experience/Card.js
+++ personal/src/components/Experience/Card.js
@@ -3,20 +3,23 @@ import './card.scss';
 
 const Card = (props) => {
   return (
-    <div className='card text-center shadow'>
-        <div className='overflow'>
-            <img src={props.img} alt='img'className='card-img-top'/>
-        </div>
-        <div className='card-body text-dark'>
-            <h4 className='card-title'>{props.title}</h4>
-            <p className='card-text text-secondary'>
-                {props.words}
-            </p>
-            <a href={props.website} className='btn btn-outline-primary'>
-                More About Company
-            </a>
+    <div className='wrapper'>
+       <div className='card text-center shadow'>
+            <div className='overflow'>
+                <img src={props.img} alt='img'className='card-img-top'/>
+            </div>
+            <div className='card-body text-dark'>
+                <h4 className='card-title'>{props.title}</h4>
+                <p className='card-text text-secondary'>
+                    {props.words}
+                </p>
+                <a href={props.website} className='btn btn-outline-primary'>
+                    More About Company
+                </a>
+            </div> 
         </div> 
     </div>
+    
   )
 }
 
